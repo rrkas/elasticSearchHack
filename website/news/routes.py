2789ccfceb373d29dc6376e8ad90e3817814d05c
ISHAN <you@example.com>
2021-05-25 14:15:55 +0530

    changes

--- website/news/routes.py
+++ website/news/routes.py
@@ -9,6 +9,7 @@ news = Blueprint('news', __name__)
 @news.route('/')
 def news_func():
     api = APINews()
-    news = get_news_from_response(api.get_latest_news()) + get_news_from_response(api.get_latest_news(kwd='Corona'))
-    news.sort(key=lambda x:x.publishedAt, reverse=True)
+    news = get_news_from_response(api.get_latest_news(
+    )) + get_news_from_response(api.get_latest_news(kwd='Corona'))
+    news.sort(key=lambda x: x.publishedAt, reverse=True)
     return render_template('home.html', news=news)
